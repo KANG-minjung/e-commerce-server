d2445978e9df2333f1db914da6bf99b93f71ac00
ming <minjung.kang@brique.co.kr>
2025-07-25 01:41:39 +0900

    User 리팩토링으로 삭제된 클래스 제거

--- src/main/java/kr/hhplus/be/server/user/usecase/UserCommandUseCase.java
+++
@@ -1,9 +0,0 @@
-package kr.hhplus.be.server.user.usecase;
-
-import kr.hhplus.be.server.user.domain.model.User;
-
-public interface UserCommandUseCase {
-    User createUser(String name);
-    void charge(Long userId, int amount);
-    void deduct(Long userId, int amount);
-}
